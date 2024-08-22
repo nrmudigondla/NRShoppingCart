33d7aaba8c49305d0cc0a5e01cdcb42214a98fe6
Narasimha Rao Mudigondla <nr.mudigondla@gmail.com>
2024-08-21 23:38:10 -0700

    initial repo creation

---
+++ ShoppingCart.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Core.Entities;
+using ShoppingCart.Infrastructure;
+
+namespace ShoppingCart.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private NRShoppingCartContext _context;
+        public ProductController(NRShoppingCartContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
---
+++ ShoppingCart.Infrastructure/NRShoppingCartContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Infrastructure
+{
+    public class NRShoppingCartContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public string DbPath { get; }
+
+        public NRShoppingCartContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "shoppingcart.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
